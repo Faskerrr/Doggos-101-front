aad73fadc8cd29d764d723a52cd615b27ef7a5c9
Steven Huth <stevenhuth@tutanota.com>
2023-03-29 19:27:54 +0200

    updated kennel_club_UK_descriptions.py to exclude wrong matches and to format output dataframe nicely

--- funcs/kennel_club_UK_descriptions.py
+++ funcs/kennel_club_UK_descriptions.py
@@ -2,6 +2,22 @@
 to get dog description data from kennel club'''
 
 import pandas as pd
+def clean_description_data(description_data):
+    ''' convert the kennel_club_uk spreadsheet to a nicer format'''
+    description_data['class'] = description_data['class'].map(str.capitalize)
+    description_data['size_of_garden'] = description_data['size_of_garden'].map(lambda x: x.replace('/ medium', ''))
+    description_data['home'] = description_data[['size_of_home', 'size_of_garden']].agg('<br>'.join, axis=1)
+    description_data['town_or_country'] = description_data['town_or_country'].map({'Either': 'Yes', 'Country': 'No'})
+    description_data['exercise'] = description_data['exercise'].map(lambda x: x.replace('per day', '').replace('Up to', '~').replace('More than', '>').replace('minutes', 'mins'))
+    description_data['grooming'] = description_data['grooming'].map(lambda x: x.replace('More than', 'More than <br>'))
+    description_data['lifespan'] = description_data['lifespan'].map(lambda x: x.replace('Over', '>').replace('Under', '<'))
+    description_data['coat_length'] = description_data['coat_length'].map(lambda x: x.replace('&', '<br> &'))
+
+    description_data.drop(columns = ['Position', 'vulnerable_native_breed', 'size_of_garden', 'size_of_home'], inplace = True)
+
+    description_data.index.name = None
+    description_data.columns = ['Class', 'Size', 'Daily exercise', 'Grooming', 'Fur length', 'Fur loss', 'Lifespan', 'City', 'Spacial needs']
+    return description_data[['Class', 'Size', 'Lifespan', 'Daily exercise', 'Spacial needs', 'City', 'Fur length', 'Fur loss', 'Grooming']]
 
 
 def clean_name(name):
@@ -53,13 +69,14 @@ def find_approximate_kennel_entries(name, description_data):
     return description_data[index_in_kennel_data]
 
 
-def exceptions(descriptions, name):
+def remove_exceptions(descriptions, name):
     ''' hard coding for unwanted matches that were not removed automatically;
     if the dog name matches a key of exceptions, the corresponding row of
     from description is dropped '''
     exceptions_dict = {'Collie': 'Border Collie'}
     if name in exceptions_dict.keys():
-        descriptions.drop(exceptions_dict[name], axis=0, inplace=True)
+        corrected_descriptions = descriptions.drop(exceptions_dict[name], axis=0)
+        return corrected_descriptions
     return descriptions
 
 
@@ -71,11 +88,14 @@ def get_description(description_data_path, species_name:str):
     If there are still no matches, looks for entries that contain any word of species_name
     '''
     description_data = pd.read_csv(description_data_path, index_col='breed_name')
+    description_data = clean_description_data(description_data)
     cleaned_name = clean_name(species_name)
     if cleaned_name in description_data.index:
         descriptions = description_data.loc[[cleaned_name],:]
-    elif not find_exact_kennel_entries(cleaned_name).empty:
-        descriptions = find_exact_kennel_entries(cleaned_name)
+    elif not find_exact_kennel_entries(description_data, cleaned_name).empty:
+        descriptions = find_exact_kennel_entries(description_data, cleaned_name)
     else:
-        descriptions = find_approximate_kennel_entries(cleaned_name)
-    return exceptions(descriptions, cleaned_name)
+        descriptions = find_approximate_kennel_entries(description_data, cleaned_name)
+    return remove_exceptions(descriptions, cleaned_name).style
+    # .style is necessary so that the output shows the "<br>" introduced in
+    #  clean_description_data as linebreaks in the dataframe
